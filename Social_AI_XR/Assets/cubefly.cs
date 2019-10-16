97250b22c944ac29567e38db8e96e1f2741f60a6
DanielPri <privorotskyd@gmail.com>
2019-10-16 15:34:45 -0400

    [#18] Integrated Head Motion into current project

---
+++ Social_AI_XR/Assets/cubefly.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+namespace FrameSynthesis.VR.Example
+{
+    public class cubefly : MonoBehaviour
+    {
+
+        void Start()
+        {
+            
+            VRGestureRecognizer.Current.NodHandler += OnNod;
+            VRGestureRecognizer.Current.HeadshakeHandler += OnHeadshake;
+        }
+
+        void OnNod()
+        {
+            gameObject.GetComponent<Rigidbody>().AddForce(0, 5, 0, ForceMode.Impulse);
+        }
+
+        void OnHeadshake()
+        {
+            gameObject.GetComponent<Rigidbody>().AddForce(5, 0, 0, ForceMode.Impulse);
+        }
+    }
+}
