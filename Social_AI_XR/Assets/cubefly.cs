01d2d1c16834a0a7cb8843ad7dcafea6ee56bb7b
Songtone <s.ongtone@hotmail.com>
2019-10-16 17:42:14 -0400

    [#18]Added text change when doing a motion

--- Social_AI_XR/Assets/cubefly.cs
+++ Social_AI_XR/Assets/cubefly.cs
@@ -1,34 +1,38 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 
 namespace FrameSynthesis.VR.Example
 {
     public class cubefly : MonoBehaviour
     {
-
+        public TextMesh textDebugger;
         void Start()
         {
             
             VRGestureRecognizer.Current.NodHandler += OnNod;
             VRGestureRecognizer.Current.HeadshakeLeftHandler += OnHeadshakeLeft;
             VRGestureRecognizer.Current.HeadshakeRightHandler += OnHeadshakeRight;
-
+            textDebugger.text = "START";
         }
 
         void OnNod()
         {
             gameObject.GetComponent<Rigidbody>().AddForce(0, 5, 0, ForceMode.Impulse);
+            textDebugger.text = "Nodding";
         }
 
         void OnHeadshakeLeft()
         {
             gameObject.GetComponent<Rigidbody>().AddForce(-5, 0, 0, ForceMode.Impulse);
+            textDebugger.text = "Head Shake Left";
         }
         void OnHeadshakeRight()
         {
             gameObject.GetComponent<Rigidbody>().AddForce(5, 0, 0, ForceMode.Impulse);
+            textDebugger.text = "Head Shake Right";
         }
     }
 }
